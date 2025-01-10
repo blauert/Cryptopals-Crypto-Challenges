518584ce8025321b2dfd1b6fc18c6e1b49d440dc
blauert <95693202+blauert@users.noreply.github.com>
2025-01-10 18:38:00 +0100

    Add Set 1

---
+++ Set 1: Basics/helpers.py
@@ -0,0 +1,119 @@
+from collections import Counter, defaultdict
+from statistics import fmean
+
+
+def xor_combination(buf1, buf2):
+    result = bytearray()
+    for b in zip(buf1, buf2):
+        result.append(b[0] ^ b[1])
+    return result
+
+
+def judge_letter_frequency(message):
+    english_letter_frequencies = {
+        ' ': 20, # space has around 20% probability
+        # https://en.wikipedia.org/wiki/Letter_frequency
+        'e': 12.702, 't': 9.056, 'a': 8.167, 'o': 7.507, 'i': 6.966, 'n': 6.749, 's': 6.327, 'h': 6.094, 'r': 5.987,
+        'd': 4.253, 'l': 4.025, 'c': 2.782, 'u': 2.758, 'm': 2.406, 'w': 2.360, 'f': 2.228, 'g': 2.015, 'y': 1.974,
+        'p': 1.929, 'b': 1.492, 'v': 0.978, 'k': 0.772, 'j': 0.153, 'x': 0.150, 'q': 0.095, 'z': 0.074
+    }
+    char_counts = Counter(message.decode("ascii", errors="ignore").lower())
+    score = 0
+    for letter in english_letter_frequencies:
+        frequency = 100 / len(message) * char_counts.get(letter, 0)
+        # sum of squared differences
+        score += (english_letter_frequencies.get(letter) - frequency)**2
+    return score
+
+
+def decypher_single_byte_xor(string):
+    results = {}
+    for ascii_char in range(128):
+        result = bytearray()
+        for b in string:
+            result.append(b ^ ascii_char)
+        results[ascii_char] = result
+    scores = []
+    for char, result in results.items():
+        # (score, char, result)
+        scores.append((judge_letter_frequency(result), char, result))
+    # return lowest scoring result
+    scores.sort()
+    return scores[0]
+
+
+def repeating_key_xor(text, key):
+    # the inverse of XOR is XOR, to this works both ways
+    i = 0
+    result = bytearray()
+    for letter in text:
+        result.append(letter ^ key[i])
+        i = (i+1) % len(key)
+    return result
+
+
+def hamming_distance(str1, str2):
+    # number of differing bits
+    count = 0
+    for b in zip(str1, str2):
+        count += bin(b[0] ^ b[1]).count('1')
+    return count
+
+
+def guess_keysize(ciphertext):
+    MAX_KEYSIZE = 40
+    MAX_BLOCKS = 10
+    # Split text into blocks
+    keysizes_blocks = defaultdict(list)
+    for keysize in range(2, min(len(ciphertext), MAX_KEYSIZE)+1):
+        num_blocks = min(MAX_BLOCKS, len(ciphertext)//keysize)
+        # take at least two keysize blocks
+        if num_blocks >= 2:
+            for b in range(num_blocks):
+                    keysizes_blocks[keysize].append(ciphertext[b*keysize : (b+1)*keysize])
+    # Find edit distances
+    edit_distances = {}
+    for keysize, blocks in keysizes_blocks.items():
+        cur_distances = []
+        for i in range(len(blocks[1:])):
+            this_block, prev_block = blocks[i], blocks[i-1]
+            dist = hamming_distance(this_block, prev_block)
+            # Normalize this result by dividing by KEYSIZE
+            normalized_dist = dist / len(this_block)
+            cur_distances.append(normalized_dist)
+        # average the distances
+        edit_distances[keysize] = fmean(cur_distances)
+    # Smallest edit distance is the key
+    smallest_keysize = min(edit_distances, key=edit_distances.get)
+    return smallest_keysize
+
+
+def break_vigenere_key(ciphertext, keysize):
+    # transpose the blocks
+    transposed = [bytearray() for _ in range(keysize)]
+    i = 0
+    for char in ciphertext:
+        transposed[i].append(char)
+        i = (i+1) % keysize
+    key = bytearray()
+    for block in transposed:
+        _, char, _ = decypher_single_byte_xor(block)
+        key.append(char)
+    return key
+
+
+if __name__ == "__main__":
+    # Letter frequency
+    not_english, english, more_english = b"##$$]ZZXX", b"X-Ray Zulu", b"ETAOIN SHRDLU"
+    print("Scores:")
+    print(not_english, "->", judge_letter_frequency(not_english))
+    print(english, "->", judge_letter_frequency(english))
+    print(more_english, "->", judge_letter_frequency(more_english))
+    print()
+    # Hamming Distance
+    str1, str2 = b"this is a test", b"wokka wokka!!!"
+    print("Hamming distance:", str1, str2, "->", hamming_distance(str1, str2))
+    print()
+    # Keysize
+    test_input = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1]
+    print("Keysize:", test_input, "->", guess_keysize(test_input))
---
+++ Set 1: Basics/set1.py
@@ -0,0 +1,115 @@
+# https://cryptopals.com/sets/1
+
+import base64
+import binascii
+import codecs
+
+from Crypto.Cipher import AES
+
+from helpers import (
+    xor_combination,
+    decypher_single_byte_xor,
+    repeating_key_xor,
+    guess_keysize,
+    break_vigenere_key,
+)
+
+
+def ch1():
+    # https://cryptopals.com/sets/1/challenges/1
+    print("1: Convert hex to base64")
+    bytes_str = bytes.fromhex('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+    print(bytes_str)
+    print(base64.b64encode(bytes_str))
+
+
+def ch2():
+    # https://cryptopals.com/sets/1/challenges/2
+    print("2: Fixed XOR")
+    buf1, buf2 = bytes.fromhex('1c0111001f010100061a024b53535009181c'), bytes.fromhex('686974207468652062756c6c277320657965')
+    result = xor_combination(buf1, buf2)
+    print(result)
+    print(binascii.hexlify(result))
+
+
+def ch3():
+    # https://cryptopals.com/sets/1/challenges/3
+    print("3: Single-byte XOR cipher")
+    string = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+    _, _, result = decypher_single_byte_xor(string)
+    print(result)
+
+
+def ch4():
+    # https://cryptopals.com/sets/1/challenges/4
+    print("4: Detect single-character XOR")
+    with open('4.txt') as file:
+        lines = [bytes.fromhex(line) for line in file.readlines()]
+    scores = []
+    for line in lines:
+        scores.append(decypher_single_byte_xor(line))
+    scores.sort()
+    _, _, result = scores[0]
+    print(result)
+
+
+def ch5():
+    # https://cryptopals.com/sets/1/challenges/5
+    print("5: Implement repeating-key XOR")
+    stanza = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    key = b'ICE'
+    print(stanza)
+    result = repeating_key_xor(stanza, key)
+    print(binascii.hexlify(result))
+
+
+def ch6():
+    # https://cryptopals.com/sets/1/challenges/6
+    print("6: Break repeating-key XOR")
+    with open('6.txt') as file:
+        ciphertext = base64.b64decode(file.read())
+    keysize = guess_keysize(ciphertext)
+    key = break_vigenere_key(ciphertext, keysize)
+    print(key)
+    plaintext = repeating_key_xor(ciphertext, key)
+    print(plaintext.decode('utf-8', errors='ignore')[:220], "...")
+
+
+def ch7():
+    # https://cryptopals.com/sets/1/challenges/7
+    print("7: AES in ECB mode")
+    key = b"YELLOW SUBMARINE"
+    with open('7.txt') as file:
+        ciphertext = base64.b64decode(file.read())
+    # https://pycryptodome.readthedocs.io/en/latest/src/cipher/classic.html#ecb-mode
+    cipher = AES.new(key, AES.MODE_ECB)
+    plaintext = cipher.decrypt(ciphertext)
+    print(plaintext.decode('utf-8', errors='ignore')[:220], "...")
+
+
+def ch8():
+    # https://cryptopals.com/sets/1/challenges/8
+    print("8: Detect AES in ECB mode")
+    with open('8.txt') as file:
+        lines = [bytes.fromhex(line.strip()) for line in file.readlines()]
+    ecb_candidates = []
+    for line_num, line in enumerate(lines):
+        line_num+=1 # match actual line numbers starting from 1
+        # split into 16-byte blocks
+        blocks = [line[i:i+16] for i in range(0, len(line), 16)]
+        num_repeated_blocks = len(blocks) - len(set(blocks))
+        ecb_candidates.append((num_repeated_blocks, line_num, line))
+    ecb_candidates.sort()
+    res = ecb_candidates[-1]
+    print(f"{res[0]} repeating blocks in line {res[1]}: {codecs.encode(res[2], 'hex')}")
+
+
+if __name__ == "__main__":
+    ch1(), print()
+    ch2(), print()
+    ch3(), print()
+    ch4(), print()
+    ch5(), print()
+    ch6(), print()
+    ch7(), print()
+    ch8()
